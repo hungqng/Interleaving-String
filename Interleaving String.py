0b4f6c9f5a6e276b84bd4d4bd2f93e7e4a586493
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-07-07 09:56:12 -0700

    Add files via upload

--- Interleaving String.py
+++ Interleaving String.py
@@ -22,3 +22,10 @@ class Solution:
                        j and s2[j-1] == s3[i+j-1] and dfs(i, j-1)))
         return  len(s1) + len(s2) == len(s3) and dfs(len(s1), len(s2))
 
+        # Solution 2
+        @lru_cache(None)
+        def dp(i, j):
+            if i == -1 and j == -1: return True
+            return (j >= 0 and s2[j] == s3[i+j+1] and dp(i, j-1)) or (i >= 0 and s1[i] == s3[i+j+1] and dp(i-1,j))
+        
+        return len(s1) + len(s2) == len(s3) and dp(len(s1) - 1, len(s2) - 1)
